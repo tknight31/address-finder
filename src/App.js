c9a8b80c2fe1891c5ffd70a6bfe80b5bf53b0b5a
Tyler Knight <tylerknight@postcodeanywhere.com>
2018-03-21 10:40:06 -0400

    minor adjustments

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Address Finder</h1>
+          <h1 className="App-title">Addressy</h1>
         </header>
         <Form />
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -21,7 +21,7 @@ export default Form;
 //       <div className="App">
 //         <header className="App-header">
 //           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Marlowe Sucks Dick</h1>
+//           <h1 className="App-title"></h1>
 //         </header>
 //         <p className="App-intro" />
 //       </div>
